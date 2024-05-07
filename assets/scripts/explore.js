c476b6524ed6dc8d38980f2f1ea0cc331d63e49d
rli128 <114562309+rli128@users.noreply.github.com>
2024-05-07 14:29:34 -0700

    explore.html

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,9 +1,7 @@
-// explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  populateVoiceList(); // Load available voices on page load
+  populateVoiceList();
 
   const pressToTalkButton = document.querySelector('button');
   const textToSpeakTextarea = document.getElementById('text-to-speak');
@@ -15,36 +13,35 @@ function init() {
     const utterance = new SpeechSynthesisUtterance(textToSpeakTextarea.value);
     const selectedVoiceURI = voiceSelect.selectedOptions[0].getAttribute('data-uri');
 
-    // Set selected voice
+
     const selectedVoice = speechSynthesis.getVoices().find(voice => voice.voiceURI === selectedVoiceURI);
     utterance.voice = selectedVoice;
 
-    // Event listeners for speech synthesis
     utterance.onstart = () => {
-      faceImage.src = 'assets/images/smiling-open.png'; // Swap image to open mouthed face
+      faceImage.src = 'assets/images/smiling-open.png';
     };
 
     utterance.onend = () => {
-      faceImage.src = 'assets/images/smiling.png'; // Swap image back to smiling face after speech ends
+      faceImage.src = 'assets/images/smiling.png';
     };
 
-    speechSynthesis.speak(utterance); // Speak the text
+    speechSynthesis.speak(utterance);
   });
 }
 
-// Function to populate voice select dropdown with available voices
+
 function populateVoiceList() {
   const speechSynthesis = window.speechSynthesis;
   const voiceSelect = document.getElementById('voice-select');
 
   speechSynthesis.addEventListener('voiceschanged', () => {
     const voices = speechSynthesis.getVoices();
-    voiceSelect.innerHTML = ''; // Clear existing options
+    voiceSelect.innerHTML = '';
 
     voices.forEach(voice => {
       const option = document.createElement('option');
       option.textContent = voice.name;
-      option.setAttribute('data-uri', voice.voiceURI); // Store voice URI as data attribute
+      option.setAttribute('data-uri', voice.voiceURI);
       voiceSelect.appendChild(option);
     });
   });
